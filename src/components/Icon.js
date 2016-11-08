4cbe6080ee8006cf9bcbb5ade833da9354317d5e
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-11-08 06:45:18 +0100

    Added some tab icons to sidebar

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,26 +1,31 @@
 import React from 'react';
 
-import { TERTIARY_COLOR, BORDER_COLOR } from '../style';
+import { TERTIARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } from '../style';
 
 let styles;
 
-export default ({image, link, size, style}) => (
-  <div style={ {
-      ...styles.container,
-      ...(image && {backgroundImage: `url(${image})`}),
-      ...(size && {width: `${size}px`, height: `${size}px`}),
-      ...style} }
-  >
-    {
-      link && (
-        <a
-          style={styles.link}
-          href={link}
-        > </a>
-      )
-    }
-  </div>
-);
+const Icon = ({image, link, size = 40, css}) => {
+  return (
+    <div style={ {
+        ...styles.container,
+        ...(image && {backgroundImage: `url(${image})`}),
+        ...(!image && {backgroundColor: SECONDARY_COLOR}),
+        ...(size && {width: `${size}px`, height: `${size}px`}),
+        ...css} }
+    >
+      {
+        link && (
+          <a
+            style={styles.link}
+            href={link}
+          > </a>
+        )
+      }
+    </div>
+  );
+}
+
+export default Icon;
 
 styles = {
   container: {
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Icon from './Icon';
 import { SCREEN_SIZE, PRIMARY_COLOR, SECONDARY_COLOR } from '../style';
 
 let styles;
@@ -11,7 +12,12 @@ export default ({ children }) => (
 );
 
 export const SideBarTabs = () => (
-  <div style={styles.tabs}></div>
+  <div style={styles.tabs}>
+  <Icon css={styles.tabIcon} />
+  <Icon css={styles.tabIcon} />
+  <Icon css={styles.tabIcon} />
+  <Icon css={styles.tabIcon} />
+  </div>
 );
 
 styles = {
@@ -24,9 +30,17 @@ styles = {
     })
   },
   tabs: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     width: '50px',
     height: '100%',
     flexGrow: 1,
     backgroundColor: PRIMARY_COLOR
+  },
+  tabIcon: {
+    width: '42px',
+    height: '42px',
+    margin: '5px 0'
   }
 };
